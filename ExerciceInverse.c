b20a3b5b9689f22c5f08d4eed003c55fda42a323
Kali07 <richardmulamba16@gmail.com>
2023-10-29 13:28:13 +0100

    update from kali

--- ExerciceInverse.c
+++ ExerciceInverse.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-/*Ecrire un programme C qui permet de lire en entrée un entier
-constitué de trois chiffres et d’afficher celui-ci inversé.
-Exemple: si l’entrée est 123 on affiche 321*/
+/*Ecrire un programme C qui permet de lire en entrï¿½e un entier
+constituï¿½ de trois chiffres et dï¿½afficher celui-ci inversï¿½.
+Exemple: si lï¿½entrï¿½e est 123 on affiche 321*/
 
 int main ()
 {
     int a,b,c,d;
     printf("entrez un entier de 3 chiffres : ");
     scanf("%d",&a);
+    c = a;
     b = (a%10)*100;
     a = a/10 ;
     b = b +(a%10)*10;
     a = a/10;
     b = b+a;
 
-    printf("Final : %d\n", b);
+    printf("le nombre %d a l'envers donne  : %d\n",c, b);
 
     return 0;
 }
