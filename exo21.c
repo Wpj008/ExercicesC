717386fde5bd55dc27d84b6ec92c6312aca5b0cd
WPJ008 <wawitephilemon@gmail.com>
2025-07-03 23:04:07 +0200

    exo21

---
+++ exo21.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//21.	Afficher les nombres de 1 à 10.
+
+
+
+int main(){
+	
+	int i;
+	
+for( i = 1 ; i <= 10 ; i++){
+	
+	printf("%d\n",i);
+}	
+	
+	return 0;
+}
