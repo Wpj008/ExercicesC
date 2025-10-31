0d9d7c38765c9ca740d01d433a000ffd48149d6c
WPJ008 <wawitephilemon@gmail.com>
2025-10-31 11:56:25 +0100

    affiche multiple de 3

---
+++ affiche_multiple_de_trois-100.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//29.	Afficher les multiples de 3 jusqu'à 100.
+
+
+
+int main(){
+	
+	
+	
+	int i;
+	
+	int a = 0;
+	
+	for(i = 0 ; i < 100 ; i += 3){
+		
+	
+		
+		printf("%d\n",i);
+	}
+	
+	
+	return 0;
+}
