aaec55113910098c03ff64f13cee4a3538677351
WPJ008 <wawitephilemon@gmail.com>
2025-10-31 12:10:29 +0100

    compte a rebours

---
+++ compte_a_rebours.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//45.	Afficher un compte à rebours de n à 1.
+
+int main(){
+	
+	int a;
+	
+	printf("Saisir un nombre : ");
+	scanf("%d",&a);
+	
+	do{
+		
+	printf("%d\n",a);	
+		
+		
+		a--;
+		
+		
+		
+		
+	}while(a > 0);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
+
+
+
